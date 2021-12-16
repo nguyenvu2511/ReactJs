a1fad8175d8fd6a963e7dd9d362fc8e6b85092ac
nguyenvu2511 <59417691+nguyenvu2511@users.noreply.github.com>
2021-12-16 07:53:04 +0700

    new

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -36,21 +36,21 @@ class Login extends Component {
         this.setState({
             errMessage: ''
         })
-        try{
+        try {
             let data = await handleLoginApi(this.state.username, this.state.password);
-            if(data && data.errCode !==0){
+            if (data && data.errCode !== 0) {
                 this.setState({
                     errMessage: data.message
                 })
             }
-            if(data && data.errCode === 0){
+            if (data && data.errCode === 0) {
                 this.props.userLoginSuccess(data.user);
                 alert('Đăng nhập thành công !');
                 console.log('login success')
             }
-        }catch(error) {
-            if(error.response) {
-                if(error.response.data){
+        } catch (error) {
+            if (error.response) {
+                if (error.response.data) {
                     this.setState({
                         errMessage: error.response.data.message
                     })
@@ -68,58 +68,51 @@ class Login extends Component {
     }
 
     handleEnterKey = (event) => {
-        if(event.keyCode === 13){
+        if (event.keyCode === 13) {
             this.handleLogin();
         }
     }
-    
+
     render() {
         return (
-            <div className ="login-background">
+            <div className="login-background">
                 <div className="login-container">
                     <div className="login-content">
                         <div className="col-12 text-login">Đăng nhập Quản trị GoodCare</div>
                         <div className="col-12 form-group login-input">
                             <label>Email:</label>
-                            <input 
-                                type="text" 
-                                className="form-control" 
-                                placeholder="Nhập Email dùng để đăng nhập..." 
-                                value={this.state.username} 
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Nhập Email dùng để đăng nhập..."
+                                value={this.state.username}
                                 onChange={(event) => this.handleOnChangeUsername(event)}
                             />
                         </div>
                         <div className="col-12 form-group login-input">
                             <label>Mật khẩu:</label>
                             <div className="custom-input-password">
-                                <input 
-                                    type={this.state.isShowPassword ? 'text' : 'password'} 
-                                    className="form-control" 
-                                    placeholder="Nhập mật khẩu..." 
-                                    onChange={(event) => {this.handleOnChangePassword(event)}}
+                                <input
+                                    type={this.state.isShowPassword ? 'text' : 'password'}
+                                    className="form-control"
+                                    placeholder="Nhập mật khẩu..."
+                                    onChange={(event) => { this.handleOnChangePassword(event) }}
                                     onKeyDown={(event) => this.handleEnterKey(event)}
                                 />
                                 <span
-                                    onClick={() => {this.handleShowHidePassword()}}
+                                    onClick={() => { this.handleShowHidePassword() }}
                                 ><i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i></span>
-                                
+
                             </div>
                         </div>
-                        <div className="col-12" style={{ color: 'red'}}>
+                        <div className="col-12" style={{ color: 'red' }}>
                             {this.state.errMessage}
                         </div>
                         <div className="col-12">
-                            <button className="btn-login" onClick={() => {this.handleLogin()}}>Đăng nhập</button>
+                            <button className="btn-login" onClick={() => { this.handleLogin() }}>Đăng nhập</button>
                         </div>
                         <div className="col-12">
                             <span className="forgot-password">Bạn đã quên mật khẩu ?</span>
-                        </div>
-                        <div className="col-12 text-center mt-3">
-                            <span className="text-other-login">Hoặc bạn có thể đăng nhập với:</span>
-                        </div>
-                        <div className="col-12 social-login">
-                            <i className="fab fa-google-plus-g google"></i>
-                            <i className="fab fa-facebook-f facebook"></i>
                         </div>
                     </div>
                 </div>
